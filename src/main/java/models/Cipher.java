b4df59eaf93cee1c0d4614f7ee4e8c7bbde1e8a6
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-26 17:18:12 +0300

    Add an array of Alphabetical letters and a property key to the class

--- src/main/java/models/Cipher.java
+++ src/main/java/models/Cipher.java
@@ -1,10 +1,13 @@
 package models;
 
+import java.lang.reflect.Array;
+
 public class Cipher {
 
     private String inputString;
     private String encodedString;
-
+    private Character[] alphabeticalLetters = {'A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' } ;
+    private int key = 11;
     public Cipher(String inputString ) {
         this.inputString = inputString;
     }
