a4a37cd865a0ffa7b845346629b2d9eec45adf3f
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-27 13:42:36 +0300

    Add a test code func for decodingSingleLetter behaviou and added a decodeFunc method to Decode.java

--- src/main/java/decode/Decode.java
+++ src/main/java/decode/Decode.java
@@ -4,8 +4,14 @@ public class Decode {
 
     private String encodedString;
     private String decodedString;
-    p
+    private Character[] alphabeticalLetters = {'A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' } ;
+    private int key = 11;
+
     public Decode(String encodedString) {
         this.encodedString = encodedString;
+    }
+
+    public String decoderFunc (){
+        return null;
     }
 }
--- src/test/java/decode/DecodeTest.java
+++ src/test/java/decode/DecodeTest.java
@@ -7,7 +7,13 @@ import static org.junit.Assert.*;
 public class DecodeTest {
     @Test
     public void newDecode_instantiatesCorrectly() {
-        Decode testDecoder = new Decode();
+        Decode testDecoder = new Decode("A");
         assertEquals(true, testDecoder instanceof Decode);
+    }
+
+    @Test
+    public void decoderFunc_decodeSingleLetter_A() {
+        Decode testDecoder = new Decode("L");
+        assertEquals("A", testDecoder.decoderFunc());
     }
 }
