cf92b848a03ce5127f8d87cc415754c79536a413
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-27 15:58:51 +0300

    Call decoder and encoder classes assign them input strings and display raw data and encoded and decoded strings

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,12 +2,20 @@ import  models.Encoder;
 import  decode.Decode;
 
 import java.io.Console;
+import java.io.IOException;
 
 public class App{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Console myConsole = System.console();
 
         System.out.println("Enter the statement to be encoded: ");
         String statementToEncode = myConsole.readLine();
+        Encoder statementEncoder = new Encoder(statementToEncode);
+        String encodedStatement = statementEncoder.getEncodedString();
+        Decode statementDecoder = new Decode(encodedStatement);
+        String decodedStatement = statementDecoder.getDecodedString();
+        System.out.println(statementToEncode);
+        System.out.println(encodedStatement);
+        System.out.println(decodedStatement);
     }
 }
